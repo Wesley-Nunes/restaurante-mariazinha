6775fe2ae0ba1d77c8fe1a910fcc92b1b78f4588
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-03-22 20:39:46 -0300

    chore: The tests are configured in the project
    resolves #9

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Home from './index'
+
+describe('Home page', () => {
+  it('should render a title', () => {
+    render(<Home />)
+
+    const title = screen.getByRole('heading')
+
+    expect(title).toBeInTheDocument()
+  })
+})
