c7df3899495ae4e95f64ae228d63982c622b6453
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-04-20 15:58:16 -0300

    fix: Generic type 'FormikProps<T>' requires 1 type argument(s)

--- src/interfaces/FieldForm.tsx
+++ src/interfaces/FieldForm.tsx
@@ -1,8 +1,9 @@
 import FormikProps from './FormikProps'
+import FormValues from './FormValues'
 
 export default interface FieldForm {
   id: string
   type: string
   labelText: string
-  formikProps: FormikProps
+  formikProps: FormikProps<FormValues>
 }
